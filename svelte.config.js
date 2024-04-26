72c816680d0c2c3cd972948b8b0d52adb9e43050
deyso <bauer.dezso@gmail.com>
2024-04-26 10:55:03 +0200

    github actions

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,8 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter({ pages: 'docs' }),
+		// adapter: adapter({ pages: 'docs' }),
+		adapter: adapter(),
 		paths: {
 			base: '/vite_test'
 		}
